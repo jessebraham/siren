60a5a4fcd261e153e007213eb85596ebfb18dab0
Jesse Braham <jesse@beta7.io>
2019-01-12 15:38:04 -0800

    Sigh...

--- siren/settings.py
+++ siren/settings.py
@@ -18,7 +18,7 @@ TWILIO_AUTH_TOKEN = config("TWILIO_AUTH_TOKEN", cast=str, default="")
 TWILIO_FROM_NUMBER = config("TWILIO_FROM_NUMBER", cast=str, default="")
 
 SIREN_SMTP_HOST = config("SIREN_SMTP_HOST", cast=str, default="")
-SIREN_SMTP_PORT = config("SIREN_SMTP_PORT", cast=int, default="")
+SIREN_SMTP_PORT = config("SIREN_SMTP_PORT", cast=int, default=0)
 SIREN_SMTP_USERNAME = config("SIREN_SMTP_USERNAME", cast=str, default="")
 SIREN_SMTP_PASSWORD = config("SIREN_SMTP_PASSWORD", cast=str, default="")
 SIREN_SMTP_FROM_ADDR = config("SIREN_SMTP_FROM_ADDR", cast=str, default="")
